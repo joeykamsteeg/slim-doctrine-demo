9c72f27ce9df1ce5181162487158b1d2d39cef9b
Joey Kamsteeg <j.kamsteeg@deroodekikker.nl>
2015-12-04 09:58:18 +0100

    configuration

--- index.php
+++ index.php
@@ -4,12 +4,32 @@
 
 	require dirname(__FILE__)."/vendor/autoload.php";
 
+
+	$config = array ();
+	switch( $_SERVER['SERVER_ADDR'] ){
+		case '::1':
+			$config['DB_DRIVER'] = 'pdo_mysql';
+			$config['DB_HOST'] = 'localhost';
+			$config['DB_USER'] = 'root';
+			$config['DB_PASS'] = 'root';
+			$config['DB_NAME'] = 'slim_doctrine';
+		break;
+
+		default:
+			$config['DB_DRIVER'] = getenv('DB_DRIVER');
+			$config['DB_HOST'] = getenv('DB_HOST');
+			$config['DB_USER'] = getenv('DB_USER');
+			$config['DB_PASS'] = getenv('DB_PASS');
+			$config['DB_NAME'] = getenv('DB_NAME');
+		break;
+	}
+
 	$app = new App( array(
-		'DB_DRIVER' => 'pdo_mysql',
-		'DB_HOST' => 'localhost',
-		'DB_USER' => 'root',
-		'DB_PASS' => 'root',
-		'DB_NAME' => 'slim_doctrine'
+		'DB_DRIVER' => $config['DB_DRIVER'],
+		'DB_HOST' => $config['DB_HOST'],
+		'DB_USER' => $config['DB_USER'],
+		'DB_PASS' => $config['DB_PASS'],
+		'DB_NAME' => $config['DB_NAME']
 	) );
 	$app->run();
 ?>
